566dde5d79c9595d30834647217de6e0b0ee3535
bubblecast <bubblecast@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-06 18:44:17 +0000

    ver 1.1
    git-svn-id: https://plugins.svn.wordpress.org/bubblecast-video-plugin/tags/1.1@152561 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bubblecast_utils.php
+++ bubblecast_utils.php
@@ -1,20 +1,27 @@
-<?
+<?php
 require_once("load_http.php");
 require_once("bubblecast_xmlparser.php");
+function is_wpmu(){
+    return function_exists('wpmu_create_user');
+}
 function bubblecast_login(){
+        $bubblecast_username = get_bubblecast_option("bubblecast_username");
+        $bubblecast_password = get_bubblecast_option("bubblecast_password");
+        $siteId = bubblecast_remote_login($bubblecast_username,$bubblecast_password);
+        update_bubblecast_option("bubblecast_site_id",$siteId);
+        return $siteId;
+}
+function bubblecast_remote_login($bubblecast_username,$bubblecast_password){
         global $authURL;
-        $bubblecast_username = get_option("bubblecast_username");
-        $bubblecast_password = get_option("bubblecast_password");
-        $xml = load($authURL."?username=".$bubblecast_username."&password=".$bubblecast_password,
+        $xml = bubblecast_load($authURL."?username=".$bubblecast_username."&password=".$bubblecast_password,
                 array('return_info'    => true));
         $user_doc = XML_unserialize($xml["body"]);
         $siteId = $user_doc["root"]["siteId"];
-        update_option("bubblecast_site_id",$siteId);
         return $siteId;
 }
     function bubblecast_send_post_data($sendPostDatURL,&$message,$link,$title){
         if(preg_match("/\\[bubblecast(.*?)\\]/",$message)){
-            load($sendPostDatURL,
+            bubblecast_load($sendPostDatURL,
                  array('method' => "post",
                 'return_info'    => false,
                 "post_data" => array (
@@ -30,5 +37,20 @@ function bubblecast_is_admin() {
     get_currentuserinfo();
     return $user_level == 10;
 }
-
+function update_bubblecast_option($opt_name,$opt_val){
+    if(is_wpmu()){
+        update_site_option($opt_name,$opt_val);
+    }
+    else{
+        update_option($opt_name,$opt_val);
+    }
+}
+function get_bubblecast_option($opt_name){
+    if(is_wpmu()){
+        return get_site_option($opt_name);
+    }
+    else{
+        return get_option($opt_name);
+    }
+}
 ?>
--- iquickcast.php
+++ iquickcast.php
@@ -1,13 +1,15 @@
 
 <script type="text/javascript">
-<?
+<?php
     require("config.php");
-    $siteId = bubblecast_login();    
+    $siteId = get_bubblecast_option("bubblecast_site_id");
+    if(!$siteId){
+        $siteId = bubblecast_login();
+    }
     global $embeddedQuickcastMovieURL, $playerMovieURL,$pluginMode;
-    $flashVars = 'siteId='.$siteId.'&amp;languages=' . get_option('bubblecast_language') . '&amp;pluginMode='.$pluginMode.'&amp;pluginUserName='.$user_login.'&amp;pluginUserEmail='.$user_email.'&amp;adminEmail='.$admin_email;
+    $flashVars = 'siteId='.$siteId.'&amp;languages=' . get_bubblecast_option('bubblecast_language') . '&amp;pluginMode='.$pluginMode.'&amp;pluginUserName='.$user_login.'&amp;pluginUserEmail='.$user_email.'&amp;adminEmail='.$admin_email;
 ?>
 function onPostInsert(html){
-//    alert("onPostInsert = " + html);
     var win = window.opener ? window.opener : window.dialogArguments;
     if ( !win )
         win = top;
@@ -16,13 +18,17 @@ function onPostInsert(html){
         tinyMCE.selectedInstance.getWin().focus();
         tinyMCE.execCommand('mceInsertContent', false, html);
     } else {
-        win.edInsertContent(win.edCanvas, html);
+	if (win.edInsertContent != null) {
+            win.edInsertContent(win.edCanvas, html);
+        } else {
+    	    // fallback to manual insert
+    	    insertAtCaret(win.document, 'content', 'post', 'content', html);
+        }
     }
 
 }
 function onCommentInsert(html){
-//    alert("insertAtCaret = " + html);
-    insertAtCaret('comment',html);
+    insertAtCaret(document, 'comment', 'commentform', 'comment', html);
 }
 </script>
 <style>
@@ -34,33 +40,13 @@ function onCommentInsert(html){
 <?php if (!$siteId): ?>
 <div align="center" style="color: red; padding: 5px;">
 <?php if (bubblecast_is_admin()): ?>
-You haven't set up Bubblecast login and password. Please, follow installation instructions to finish setup in your administration console
+<?php _e('You haven\'t set up Bubblecast login and password. Please, follow installation instructions to finish setup in your administration console at <b>Site Admin -&gt; Settings -&gt; Bubblecast</b>', 'bubblecast');?>
 <?php else: ?>
-Bubblecast plugin is not configured properly. Please, contact administrator.
+<?php _e('Bubblecast plugin is not configured properly. Please, contact administrator.', 'bubblecast');?>
 <?php endif; ?>
 </div>
 <?php else: ?>
-<a href="http://bubble-cast.com">http://bubble-cast.com</a>
+<div><a href="http://bubble-cast.com">http://bubble-cast.com</a></div>
 <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0"                width="475" height="375" id="quickcast" align="middle">            <param name="allowScriptAccess" value="always" />            <param name="movie" value="<? echo $embeddedQuickcastMovieURL;?>" />            <param name="flashvars" value="<? echo $flashVars; ?>" />            <param name="quality" value="high" />            <param name="allowfullscreen" value="true"/>            <param name="bgcolor" value="#ededed" />                <embed src="<? echo $embeddedQuickcastMovieURL;?>"                       quality="high" bgcolor="#ededed" width="475" height="375" name="quickcast"                       flashvars="<? echo $flashVars; ?>" allowfullscreen="true"                       align="middle" allowScriptAccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />        </object>
 <?php endif; ?>
 </div>
-<script type="text/javascript">
-    var wtop = window.top ? window.top : window.parent;
-			    wtop.jQuery("#TB_window").css(
-    				{
-    			        width: "510px",
-    			        height: "415px"
-
-    		      	}
-
-    			);
-			    wtop.jQuery("#TB_iframeContent").css(
-    				{
-    			        width: "510px",
-    			        height: "415px"
-
-    		      	}
-
-    			);
-
-</script>
