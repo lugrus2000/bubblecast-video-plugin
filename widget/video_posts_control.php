2de35ea72ca53da90723d3f2090dbfae2f28ca3c
bubblecast <bubblecast@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-13 20:45:24 +0000

    removed debug output
    git-svn-id: https://plugins.svn.wordpress.org/bubblecast-video-plugin/tags/1.1.1@154465 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- widget/video_posts_control.php
+++ widget/video_posts_control.php
@@ -23,10 +23,7 @@
     if(empty($options)){
         $options = array(); 
     }
-print_r($defaultOptions);
     $options = array_merge($defaultOptions, $options);
-    echo "uuu";
-print_r($options);
     $categories = get_categories(array('hide_empty' => false));
 ?>
     <p>
